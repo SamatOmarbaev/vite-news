62c993df6d7c38cadcd1deaac622729b69ec22bd
Samat <xzwqt@mail.ru>
2024-01-31 18:37:19 +0700

    fix

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,14 +1,15 @@
-import { Article } from "../../model/types/Article"
+// import { Article } from "../../model/types/Article"
 import { memo } from "react"
 import './ArticleDetails.css'
 import { useParams } from "react-router-dom"
 
-interface ArticleDetailsProps {
-  item?: Article
-}
+// interface ArticleDetailsProps {
+//   item?: Article
+// }
 
-export const ArticleDetails = memo((props: ArticleDetailsProps) => {
-  const {item} = props
+// export const ArticleDetails = memo((props: ArticleDetailsProps) => {
+export const ArticleDetails = memo(() => {
+  // const {item} = props
   const {id} = useParams()
   
   return (
