a36ad9e55d05cf419e06ba95ca1df7f475dddbb3
Samat <xzwqt@mail.ru>
2024-02-14 20:02:38 +0700

    fix

--- src/features/ui/model/selectors/getUiSelector.ts
+++ src/features/ui/model/selectors/getUiSelector.ts
@@ -1,9 +1,9 @@
-import { createSelector } from "@reduxjs/toolkit";
+// import { createSelector } from "@reduxjs/toolkit";
 import { UiState } from "../types/UiSchema";
 
 export const getUIScroll = (state: UiState) => state.scroll
-export const getUIScrollPath = createSelector(
-  getUIScroll,
-  (state: UiState, path: string) => path,
-  (scroll, path) => scroll[path] || 0,
-)
+// export const getUIScrollPath = createSelector(
+//   getUIScroll,
+//   (state: UiState, path: string) => path,
+//   (scroll, path) => scroll[path] || 0,
+// )
