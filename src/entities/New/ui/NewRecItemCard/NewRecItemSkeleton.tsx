27b9fa59ca59e96abc1ecc02d49c9311fd208acd
Samat <xzwqt@mail.ru>
2024-02-15 16:04:52 +0700

    add react-virtuoso

--- src/entities/New/ui/NewRecItemCard/NewRecItemSkeleton.tsx
+++ src/entities/New/ui/NewRecItemCard/NewRecItemSkeleton.tsx
@@ -1,10 +1,11 @@
 import { Skeleton, Stack } from "@mui/material"
 import { memo } from "react"
+import cls from './NewRecItemCard.module.css'
 
 export const NewRecItemSkeleton = memo(() => {
   return (
     <Stack>
-      <Skeleton variant="rounded" width={400} height={125} />
+      <Skeleton className={cls.skeleton} variant="rounded" width={400} height={125} />
     </Stack>
   )
 })
